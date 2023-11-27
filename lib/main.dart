1a207730006deb9f0652a15eeffeb325b3e53ac1
HadM10 <hadmak10@gmail.com>
2023-11-27 18:41:56 +0200

    lastcommit

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Recipe Generator',
       theme: ThemeData(
         primarySwatch: Colors.red,
